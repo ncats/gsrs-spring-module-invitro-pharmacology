177430ec2314e267e715bdd8e247a9b8c28157f1
Newatia <Archana.Newatia@fda.gov>
2026-03-23 12:15:13 -0400

    revert data type

--- gsrs-module-invitro-pharmacology-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/invitropharmacology/models/InvitroAssayResult.java
+++ gsrs-module-invitro-pharmacology-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/invitropharmacology/models/InvitroAssayResult.java
@@ -39,7 +39,7 @@ public class InvitroAssayResult extends InvitroPharmacologyCommanData {
     public String testAgentConcentrationUnits;
 
     @Column(name = "RESULT_VALUE")
-    public String resultValue;
+    public Double resultValue;
 
     @Column(name = "RESULT_VALUE_UNITS")
     public String resultValueUnits;
